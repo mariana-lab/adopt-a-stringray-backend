ca90d1c9f958727b766d5cf3f103c25d13a3aceb
Mariana <mariana.barros@academiadecodigo.org>
2020-09-28 22:49:22 +0000

    last changes

--- routes/stringray.route.js
+++ routes/stringray.route.js
@@ -4,10 +4,11 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const stringray_controller = require('../controllers/stringray.controller');
 
-router.post('/create', stringray_controller.stringray_create);
-router.post('/create-many', stringray_controller.stringray_create_many);
 router.get('/', stringray_controller.stringrays);
 router.get('/:id', stringray_controller.stringray_details);
+
+router.post('/create', stringray_controller.stringray_create);
+router.post('/create-many', stringray_controller.stringray_create_many);
 router.put('/:id/update', stringray_controller.stringray_update);
 router.delete('/:id/delete', stringray_controller.stringray_delete);
 module.exports = router;
--- routes/vimdiesel.route.js
+++ routes/vimdiesel.route.js
@@ -4,10 +4,11 @@ const router = express.Router();
 // Require the controllers WHICH WE DID NOT CREATE YET!!
 const vimdiesel_controller = require('../controllers/vimdiesel.controller');
 
-router.post('/create', vimdiesel_controller.vimdiesel_create);
-router.post('/create-many', vimdiesel_controller.vimdiesel_create_many);
 router.get('/', vimdiesel_controller.vimdiesels);
 router.get('/:id', vimdiesel_controller.vimdiesel_details);
+
+router.post('/create', vimdiesel_controller.vimdiesel_create);
+router.post('/create-many', vimdiesel_controller.vimdiesel_create_many);
 router.put('/:id/update', vimdiesel_controller.vimdiesel_update);
 router.delete('/:id/delete', vimdiesel_controller.vimdiesel_delete);
 
